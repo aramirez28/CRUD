eb0614e5ec9d026380940b3d0e46ed14ff3c7f60
AlexisR28 <AlexisR28>
2021-10-25 09:48:32 -0500

    Se agrega el proyecto

---
+++ demo/src/main/java/com/example/demo/Models/AttributeModel.java
@@ -0,0 +1,14 @@
+package com.example.demo.Models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AttributeModel {
+    
+    private long Id;
+    private int Key;
+    private int value;
+
+}
---
+++ demo/src/main/java/com/example/demo/Models/CardBin.java
@@ -0,0 +1,28 @@
+package com.example.demo.Models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+@Entity
+@Table(name="tbl_Card_Bind")  
+@Getter
+@Setter
+public class CardBin {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private long Id;
+    private int binNumber;
+    private String binType;
+
+    /*@ManyToOne
+    private Attribute Attribute;*/
+
+
+}
+
